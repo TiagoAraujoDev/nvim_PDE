d6495d96a54af2315d1e1a83be62a5530d6dcf73
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-08-17 15:59:41 +0100

    refactor: autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,26 +1,34 @@
+local autocmd = vim.api.nvim_create_autocmd
+local autogroup = vim.api.nvim_create_augroup
+local cmd = vim.cmd
+
 local dashboard = require "alpha.themes.dashboard"
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  desc = "Close some windows with 'q'",
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
-  callback = function()
-    vim.cmd [[
-      nnoremap <silent> <buffer> q :close<CR>
-      set nobuflisted
-    ]]
+autocmd("BufWinEnter", {
+  desc = "Make some windows close with q",
+  group = autogroup("quit_q", { clear = true }),
+  callback = function(event)
+    local filetype = vim.api.nvim_get_option_value("filetype", { buf = event.buf })
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = event.buf })
+    if buftype == "nofile" or filetype == "help" then
+      vim.keymap.set("n", "q", "<cmd>close<cr>", {
+        desc = "Close window",
+        buffer = event.buf,
+        silent = true,
+        nowait = true,
+      })
+    end
   end,
 })
 
+local alpha_settings = autogroup("alpha_footer", { clear = true })
 vim.api.nvim_create_autocmd("UIEnter", {
   desc = "Configure Alpha footer",
+  group = alpha_settings,
   callback = function()
-    -- HACK: set an autocmd to disable statusline
-    vim.opt.laststatus = 0
-
     local get_icon = require("utils.icons").get_icon
     local stats = require("lazy").stats()
     local ms = string.format("%.2f", stats.startuptime)
-
     dashboard.section.footer.val = {
       " ",
       " ",
@@ -28,12 +36,35 @@ vim.api.nvim_create_autocmd("UIEnter", {
       get_icon("Bolt", 1) .. "Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms " .. get_icon "Clock",
     }
     dashboard.section.footer.opts.hl = "DashboardFooter"
-    pcall(vim.cmd.AlphaRedraw)
+    pcall(cmd.AlphaRedraw)
+  end,
+})
+autocmd({ "User", "BufEnter" }, {
+  desc = "Disable statusline for alpha",
+  group = alpha_settings,
+  callback = function(event)
+    if
+      (
+        (event.event == "User" and event.file == "AlphaReady")
+        or (event.event == "BufEnter" and vim.api.nvim_get_option_value("filetype", { buf = event.buf }) == "alpha")
+      ) and not vim.g.before_alpha
+    then
+      vim.g.before_alpha = { laststatus = vim.opt.laststatus:get() }
+      vim.opt.laststatus = 0
+    elseif
+      vim.g.before_alpha
+      and event.event == "BufEnter"
+      and vim.api.nvim_get_option_value("buftype", { buf = event.buf }) ~= "nofile"
+    then
+      vim.opt.laststatus = vim.g.before_alpha.laststatus
+      vim.g.before_alpha = nil
+    end
   end,
 })
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
+autocmd("FileType", {
   desc = "Set wordwrap and spell checking in git and markdown files",
+  group = autogroup("wordwrap_spell", { clear = true }),
   pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
@@ -41,21 +72,24 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "VimResized" }, {
+autocmd("VimResized", {
   desc = "Resize tab on window resize",
+  group = autogroup("window_resize", { clear = true }),
   callback = function()
-    vim.cmd "tabdo wincmd ="
+    cmd "tabdo wincmd ="
   end,
 })
 
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+autocmd("TextYankPost", {
   desc = "Configure highlight group and timeout for yank command",
+  group = autogroup("highlightyank", { clear = true }),
+  pattern = "*",
   callback = function()
     vim.highlight.on_yank { higroup = "IncSearch", timeout = 150 }
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+autocmd("BufWritePost", {
   desc = "Refresh codelens when open a java file",
   pattern = { "*.java" },
   callback = function()
@@ -63,76 +97,24 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+autocmd("BufWinEnter", {
+  desc = "Pause Illuminate on very big files",
+  group = autogroup("illuminate_stop", { clear = true }),
   callback = function()
     local line_count = vim.api.nvim_buf_line_count(0)
     if line_count >= 5000 then
-      vim.cmd "IlluminatePauseBuf"
+      cmd "IlluminatePauseBuf"
     end
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  desc = "Set formatoptions to not autocomment on new line",
+autocmd("BufWinEnter", {
+  desc = "Disable automatic comment on insert new line above/bellow with O/o",
+  group = autogroup("auto_comment", { clear = true }),
   callback = function()
-    vim.cmd "set formatoptions-=cro"
+    cmd "set formatoptions-=cro"
   end,
 })
 
--- HACK:
--- Illuminate highlight Fix
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    vim.cmd [[ 
-      hi LspReferenceRead gui=NONE guibg=#424A56
-    ]]
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    vim.cmd [[ 
-      hi LspReferenceWrite gui=NONE guibg=#424A56
-    ]]
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    vim.cmd [[ 
-      hi LspReferenceText gui=NONE guibg=#424A56
-    ]]
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    vim.cmd [[ 
-      hi IlluminatedWordRead gui=NONE guibg=#424A56
-    ]]
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    vim.cmd [[ 
-      hi IlluminatedWordWrite gui=NONE guibg=#424A56
-    ]]
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    vim.cmd [[ 
-      hi IlluminatedWordText gui=NONE guibg=#424A56
-    ]]
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    vim.cmd [[ 
-      hi link illuminatedWord LspReferenceText
-    ]]
   end,
 })
