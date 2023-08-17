a2490e09efeabedd7b5abbfd8b5331b9ce278f18
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-08-17 17:33:59 +0100

    override some icons

--- lua/user/nvim-dev-icons.lua
+++ lua/user/nvim-dev-icons.lua
@@ -1,10 +1,11 @@
 local M = {
   "nvim-tree/nvim-web-devicons",
   event = "VeryLazy",
-  commit = "e283ab937e0197b37ec5d8013e49495193407324"
+  commit = "e283ab937e0197b37ec5d8013e49495193407324",
 }
 
 function M.config()
+  local get_icon = require("utils.icons").get_icon
   require("nvim-web-devicons").setup {
     override = {
       zsh = {
@@ -16,6 +17,80 @@ function M.config()
     },
     color_icons = true,
     default = true,
+    override_by_filename = {
+      [".gitignore"] = {
+        icon = get_icon "Git",
+        color = "#f1502f",
+        name = "Gitignore",
+      },
+      [".luacheckrc"] = {
+        icon = get_icon "Lua",
+        color = "#51A0CF",
+        name = "Luacheck",
+      },
+      [".eslintrc.json"] = {
+        icon = get_icon "Eslint",
+        color = "#4b32c3",
+        name = "Eslintrc",
+      },
+      [".eslintignore"] = {
+        icon = get_icon "Eslint",
+        color = "#4b32c3",
+        name = "Eslintignore",
+      },
+      [".env.example"] = {
+        icon = get_icon "Env",
+        color = "#fcf003",
+        name = "Env_example",
+      },
+      [".env.production"] = {
+        icon = get_icon "Env",
+        color = "#fcf003",
+        name = "Env_prod",
+      },
+      [".env.development"] = {
+        icon = get_icon "Env",
+        color = "#fcf003",
+        name = "Env_dev",
+      },
+    },
+    override_by_extension = {
+      ["json"] = {
+        icon = get_icon "Json",
+        color = "#fcf003",
+        name = "JSON",
+      },
+      ["java"] = {
+        icon = get_icon "Coffe",
+        color = "#FB1A00",
+        name = "Java",
+      },
+      ["class"] = {
+        icon = get_icon "Coffe",
+        color = "#4A61EA",
+        name = "Java_bytecode",
+      },
+      ["js"] = {
+        icon = get_icon "Javascript",
+        color = "#F0DB4F",
+        name = "Javascript",
+      },
+      ["mjs"] = {
+        icon = get_icon "Javascript",
+        color = "#F0DB4F",
+        name = "Michael_Jackson_script",
+      },
+      ["ts"] = {
+        icon = get_icon "Typescript",
+        color = "#007acc",
+        name = "Typescript",
+      },
+      ["astro"] = {
+        icon = get_icon("Astro"),
+        color = "#FF6900",
+        name = "Astro",
+      },
+    },
   }
 end
 
