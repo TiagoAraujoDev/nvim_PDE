efa53d5ef808eb064e539926ace46a2187b1b8af
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-08-17 16:00:29 +0100

    chore: add highlights for Lsp and Illuminate

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -1,32 +1,44 @@
+local hl = vim.api.nvim_set_hl
+
+-- Illuminate
+hl(0, "LspReferenceRead", { bg = "#424A56", underline = false })
+hl(0, "LspReferenceWrite", { bg = "#424A56", underline = false })
+hl(0, "LspReferenceText", { bg = "#424A56", underline = false })
+hl(0, "IlluminatedWordRead", { bg = "#424A56", underline = false })
+hl(0, "IlluminatedWordWrite", { bg = "#424A56", underline = false })
+hl(0, "IlluminatedWordText", { bg = "#424A56", underline = false })
+hl(0, "IlluminatedWordWrite", { bg = "#424A56", underline = false })
+hl(0, "IlluminatedWordWrite", { bg = "#424A56", underline = false })
+hl(0, "IlluminatedWordWrite", { bg = "#424A56", underline = false })
+
 -- lualine hl
-vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = "#e3c817" })
+hl(0, "DiagnosticWarn", { fg = "#e3c817" })
 
 -- Noice
-vim.api.nvim_set_hl(0, "NoiceCmdlineLua", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlineHelp", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlineInput", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlineFilter", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlineIconLua", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlineIconHelp", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlineIconInput", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlineIconFilter", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlinePopupTitle", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorder", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlineIcon", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlineIconSearch", { fg = "#ff9e3b", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderSearch", { fg = "#ff9e3b", bg = "#181616" })
-vim.api.nvim_set_hl(0, "NoiceMini", { bg = "#181616" })
+hl(0, "NoiceCmdlineLua", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlineHelp", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlineInput", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlineFilter", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlineIconLua", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlineIconHelp", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlineIconInput", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlineIconFilter", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlinePopupTitle", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlinePopupBorder", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlineIcon", { fg = "#658594", bg = "#181616" })
+hl(0, "NoiceCmdlineIconSearch", { fg = "#ff9e3b", bg = "#181616" })
+hl(0, "NoiceCmdlinePopupBorderSearch", { fg = "#ff9e3b", bg = "#181616" })
+hl(0, "NoiceMini", { bg = "#181616" })
 
 -- Editor
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#181616", fg = "#54546d" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#181616" })
+hl(0, "FloatBorder", { bg = "#181616", fg = "#54546d" })
+hl(0, "NormalFloat", { bg = "#181616" })
 
 -- Telescope
-vim.api.nvim_set_hl(0, "TelescopeSelection", { fg = "#ff9e3b", bg = "#181616" })
-vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { fg = "#ff9e3b", bg = "#181616" })
+hl(0, "TelescopeSelection", { fg = "#ff9e3b", bg = "#181616" })
+hl(0, "TelescopeSelectionCaret", { fg = "#ff9e3b", bg = "#181616" })
 
 -- Trouble
-vim.api.nvim_set_hl(0, "TroubleFoldIcon", { fg = "#ff9e3b", bg = "#181616" })
-vim.api.nvim_set_hl(0, "TroubleLocation", { fg = "#658594", bg = "#181616" })
-vim.api.nvim_set_hl(0, "TroubleCount", { fg = "#ff9e3b", bg = "#181616" })
-
+hl(0, "TroubleFoldIcon", { fg = "#ff9e3b", bg = "#181616" })
+hl(0, "TroubleLocation", { fg = "#658594", bg = "#181616" })
+hl(0, "TroubleCount", { fg = "#ff9e3b", bg = "#181616" })
